6b4cc33038623d5242c79aa966437f3cc0a9dc4f
Han Pengbo <nimingzhe2008@gmail.com>
2014-12-01 17:36:38 +0800

    完善了poi

--- My_Baidu_Map/ViewController.h
+++ My_Baidu_Map/ViewController.h
@@ -9,10 +9,24 @@
 #import <UIKit/UIKit.h>
 #import "BMapKit.h"
 
-@interface ViewController : UIViewController <BMKMapViewDelegate,BMKLocationServiceDelegate,BMKGeoCodeSearchDelegate>
+#define searchFlag 1
+#define getCurrentLocationFlag 2
+
+@interface ViewController : UIViewController <BMKMapViewDelegate,BMKLocationServiceDelegate,BMKGeoCodeSearchDelegate,BMKPoiSearchDelegate,UITextFieldDelegate>
 {
+    //定位
     BMKLocationService* _locService;
+    
+    //地理编码
     BMKGeoCodeSearch* _geoCodeSearch;
+    NSString *_currentCity;
+    int geoCodeFlag;
+    
+    //poi检索
+    int _curPage;
+    BMKPoiSearch* _poiSearch;
+    NSArray *_poiArray;
+    NSInteger _currentPoiIndex;
 }
 @property (weak, nonatomic) IBOutlet BMKMapView *mapView;
 @property (weak, nonatomic) IBOutlet UIButton *genSuiButton;
@@ -20,13 +34,17 @@
 @property (weak, nonatomic) IBOutlet UIButton *luKuangButton;
 @property (weak, nonatomic) IBOutlet UITextField *searchTextField;
 
+@property (weak, nonatomic) IBOutlet UIView *detailView;
+@property (weak, nonatomic) IBOutlet UILabel *poiAddressLabel;
+@property (weak, nonatomic) IBOutlet UIView *poiDetailView;
 
 - (IBAction)genSuiAction:(id)sender;
 - (IBAction)luoPanAction:(id)sender;
 - (IBAction)zoomInAction:(id)sender;
 - (IBAction)zoomOutAction:(id)sender;
 - (IBAction)luKuangAction:(id)sender;
-- (IBAction)searchAction:(id)sender;
+- (IBAction)goToPoiAction:(id)sender;
+- (IBAction)nextPoiAction:(id)sender;
 
 @end
 
---
+++ My_Baidu_Map/ViewController.m
@@ -0,0 +1,279 @@
+//
+//  ViewController.m
+//  My Baidu Map
+//
+//  Created by work on 14/11/29.
+//  Copyright (c) 2014年 韩鹏博. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.searchTextField.delegate=self;
+    
+    self.mapView.zoomLevel=18;
+    
+    _locService = [[BMKLocationService alloc] init];
+    
+    [_locService startUserLocationService];
+    
+    _mapView.showsUserLocation = NO;
+    _mapView.userTrackingMode = BMKUserTrackingModeFollow;
+    _mapView.showsUserLocation = YES;
+    
+    _geoCodeSearch=[[BMKGeoCodeSearch alloc] init];
+    
+    _poiSearch=[[BMKPoiSearch alloc] init];
+    
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [_mapView viewWillAppear];
+    _mapView.delegate = self; // 此处记得不用的时候需要置nil，否则影响内存的释放
+    _locService.delegate = self;
+    _geoCodeSearch.delegate=self;
+    _poiSearch.delegate = self;
+    
+}
+
+
+-(void)viewWillDisappear:(BOOL)animated
+{
+    [_mapView viewWillDisappear];
+    _mapView.delegate = nil; // 不用时，置nil
+    _locService.delegate = nil;
+    _geoCodeSearch.delegate=nil;
+    _poiSearch.delegate = nil;
+}
+
+
+- (IBAction)genSuiAction:(id)sender {
+    self.luoPanButton.hidden=NO;
+    self.genSuiButton.hidden=YES;
+    
+    self.mapView.showsUserLocation=NO;
+    self.mapView.userTrackingMode=BMKUserTrackingModeFollow;
+    self.mapView.showsUserLocation=YES;
+}
+
+- (IBAction)luoPanAction:(id)sender {
+    self.luoPanButton.hidden=YES;
+    self.genSuiButton.hidden=NO;
+    
+    self.mapView.showsUserLocation=NO;
+    self.mapView.userTrackingMode=BMKUserTrackingModeFollowWithHeading;
+    self.mapView.showsUserLocation=YES;
+}
+
+- (IBAction)zoomInAction:(id)sender {
+    [self.mapView zoomIn];
+}
+
+- (IBAction)zoomOutAction:(id)sender {
+    [self.mapView zoomOut];
+}
+
+- (IBAction)luKuangAction:(id)sender {
+    if (self.luKuangButton.tag==0) {
+        [_mapView setMapType:BMKMapTypeTrafficOn];
+        self.luKuangButton.tag=1;
+        [self.luKuangButton setTitle:@"关闭路况图" forState:UIControlStateNormal];
+    }
+    else
+    {
+        [_mapView setMapType:BMKMapTypeStandard];
+        self.luKuangButton.tag=0;
+        [self.luKuangButton setTitle:@"打开路况图" forState:UIControlStateNormal];
+    }
+    
+}
+
+
+
+- (IBAction)goToPoiAction:(id)sender {
+}
+
+- (IBAction)nextPoiAction:(id)sender {
+    _currentPoiIndex++;
+    if (_currentPoiIndex<[_poiArray count]) {
+        BMKPoiInfo* tempPoi=_poiArray[_currentPoiIndex];
+        self.poiAddressLabel.text=tempPoi.address;
+        _mapView.centerCoordinate = tempPoi.pt;
+        NSLog(@"tempoi.pt x:%f,y:%f",tempPoi.pt.latitude,tempPoi.pt.longitude);
+        NSLog(@"_mapView.centerCoordinate x:%f,y:%f",_mapView.centerCoordinate.latitude,_mapView.centerCoordinate.longitude);
+    }
+    else
+    {
+        _currentPoiIndex=0;
+        BMKPoiInfo* tempPoi=_poiArray[_currentPoiIndex];
+        self.poiAddressLabel.text=tempPoi.address;
+        _mapView.centerCoordinate = tempPoi.pt;
+        NSLog(@"tempoi.pt x:%f,y:%f",tempPoi.pt.latitude,tempPoi.pt.longitude);
+        NSLog(@"_mapView.centerCoordinate x:%f,y:%f",_mapView.centerCoordinate.latitude,_mapView.centerCoordinate.longitude);
+    }
+    
+    
+    
+}
+
+#pragma mark UITextFieldDelegate
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [self.searchTextField endEditing:YES];
+    
+    BMKReverseGeoCodeOption *reverseGeocodeSearchOption = [[BMKReverseGeoCodeOption alloc]init];
+    reverseGeocodeSearchOption.reverseGeoPoint = _locService.userLocation.location.coordinate;
+    
+    BOOL flag = [_geoCodeSearch reverseGeoCode:reverseGeocodeSearchOption];
+    
+    if(flag)
+    {
+        NSLog(@"反geo检索发送成功");
+    }
+    else
+    {
+        NSLog(@"反geo检索发送失败");
+    }
+    geoCodeFlag=searchFlag;
+    
+    return YES;
+}
+
+#pragma mark implement BMKSearchDelegate
+- (void)onGetPoiResult:(BMKPoiSearch *)searcher result:(BMKPoiResult*)result errorCode:(BMKSearchErrorCode)error
+{
+    // 清除屏幕中所有的annotation
+    NSArray* array = [NSArray arrayWithArray:_mapView.annotations];
+    [_mapView removeAnnotations:array];
+    
+    if (error == BMK_SEARCH_NO_ERROR) {
+        self.poiDetailView.hidden=NO;
+        _poiArray=result.poiInfoList;
+        for (int i = 0; i < result.poiInfoList.count; i++) {
+            BMKPoiInfo* poi = [result.poiInfoList objectAtIndex:i];
+            BMKPointAnnotation* item = [[BMKPointAnnotation alloc]init];
+            item.coordinate = poi.pt;
+            item.title = poi.name;
+            [_mapView addAnnotation:item];
+            if(i == 0)
+            {
+                //将第一个点的坐标移到屏幕中央
+                _mapView.centerCoordinate = poi.pt;
+                self.poiAddressLabel.text=poi.address;
+                _currentPoiIndex=0;
+            }
+            
+        }
+        
+    } else if (error == BMK_SEARCH_AMBIGUOUS_ROURE_ADDR){
+        NSLog(@"起始点有歧义");
+    } else {
+        // 各种情况的判断。。。
+    }
+}
+
+#pragma mark BMKGeoCodeSearchDelegate
+
+- (void)onGetGeoCodeResult:(BMKGeoCodeSearch *)searcher result:(BMKGeoCodeResult *)result errorCode:(BMKSearchErrorCode)error{
+    if (error == BMK_SEARCH_NO_ERROR) {
+        //在此处理正常结果
+        NSLog(@"地址：%@",result.address);
+    }
+    else {
+        NSLog(@"抱歉，未找到结果");
+    }
+}
+
+- (void)onGetReverseGeoCodeResult:(BMKGeoCodeSearch *)searcher result:(BMKReverseGeoCodeResult *)result errorCode:(BMKSearchErrorCode)error;
+{
+    if (error == BMK_SEARCH_NO_ERROR) {
+        switch (geoCodeFlag) {
+            case searchFlag:
+            {
+                //获得当前城市
+                if (result.address==nil) {
+                    NSLog(@"无地址");
+                    return;
+                }
+                NSArray *firstArray=[result.address componentsSeparatedByString:@"省"];
+                NSArray *cityComp=[(NSString*)firstArray[1] componentsSeparatedByString:@"市"];
+                _currentCity=cityComp[0];
+                //搜索地点
+                BMKCitySearchOption *citySearchOption = [[BMKCitySearchOption alloc]init];
+                citySearchOption.pageIndex = _curPage;
+                citySearchOption.pageCapacity = 10;
+                citySearchOption.city= _currentCity;
+                citySearchOption.keyword = self.searchTextField.text;
+                NSLog(@"城市：%@，关键字：%@,_curpage:%d",citySearchOption.city,citySearchOption.keyword,_curPage);
+                BOOL flag = [_poiSearch poiSearchInCity:citySearchOption];
+                
+                if(flag)
+                {
+                    
+                    NSLog(@"城市内检索发送成功");
+                }
+                else
+                {
+                    
+                    NSLog(@"城市内检索发送失败");
+                }
+                
+            }
+                break;
+            
+            case getCurrentLocationFlag:
+            {
+                
+            }
+                break;
+                
+            default:
+                break;
+        }
+        
+        
+    }
+    else {
+        NSLog(@"抱歉，未找到结果");
+    }
+}
+
+
+#pragma mark BMKLocationServiceDelegate
+
+- (void)willStartLocatingUser
+{
+    NSLog(@"willStartLocatingUser");
+}
+
+- (void)didStopLocatingUser
+{
+    NSLog(@"didStopLocatingUser");
+}
+
+- (void)didUpdateUserHeading:(BMKUserLocation *)userLocation
+{
+    [_mapView updateLocationData:userLocation];
+    
+    
+    
+}
+
+- (void)didUpdateUserLocation:(BMKUserLocation *)userLocation
+{
+    
+    [_mapView updateLocationData:userLocation];
+    
+}
+
+
+@end
